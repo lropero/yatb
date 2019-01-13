a18bfc22c261bc129d61980788459aef6252c02f
Luciano Ropero <lropero@gmail.com>
2019-01-13 12:23:25 -0300

    Minor

--- strategies/vsa/index.js
+++ strategies/vsa/index.js
@@ -11,7 +11,6 @@ class Strategy {
         return resolve(signals)
       }
       if (candles[0].low === Math.min(...(candles.slice(0, Math.min(20, candles.length)).map((candle) => candle.low)))) { // Last candle is a local low
-        signals.push('CLOSE SHORT')
         if (candles[0].volume === Math.max(...(candles.slice(0, Math.min(10, candles.length)).map((candle) => candle.volume)))) { // Volume larger than previous candles
           if (candles[0].range === Math.max(...(candles.slice(0, Math.min(10, candles.length)).map((candle) => candle.range)))) { // Range larger than previous candles
             if ((candles[0].close - candles[0].low) / (candles[0].high - candles[0].low) > 0.5) { // Price rejection
@@ -20,7 +19,6 @@ class Strategy {
           }
         }
       } else if (candles[0].high === Math.max(...(candles.slice(0, Math.min(20, candles.length)).map((candle) => candle.high)))) { // Last candle is a local high
-        signals.push('CLOSE LONG')
         if (candles[0].volume === Math.max(...(candles.slice(0, Math.min(10, candles.length)).map((candle) => candle.volume)))) { // Volume larger than previous candles
           if (candles[0].range === Math.max(...(candles.slice(0, Math.min(10, candles.length)).map((candle) => candle.range)))) { // Range larger than previous candles
             if ((candles[0].high - candles[0].close) / (candles[0].high - candles[0].low) > 0.5) { // Price rejection
